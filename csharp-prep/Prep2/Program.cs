f088f8a86dd4c9520291985a51c8188dab841a88
Algon8364j <104524754+Algon8364j@users.noreply.github.com>
2023-02-24 03:39:04 +0000

    prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+       Console.Write("What is Your Grade Percentage? ");
+       string responce =Console.ReadLine();
+       int percent = int.Parse(responce);
+        string grade = "";
+        if(percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        } 
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your Grade is: {grade}");
+        if (percent >= 70)
+        {
+            Console.WriteLine("You Passed");
+        }
+        else
+        {
+            Console.WriteLine("Try Again");
+        }
     }
 }
