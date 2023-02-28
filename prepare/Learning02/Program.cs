6041beb1b6ce24063bf4f8c16e81a1d3e0c4ca5d
Algon8364j <104524754+Algon8364j@users.noreply.github.com>
2023-02-28 21:29:55 +0000

    reusm

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -16,12 +16,5 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
-
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
-
-        myResume.Display();
     }
 }
